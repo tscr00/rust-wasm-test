f9dd02768cb3070e787e432c10981b33f1a609b4
dev <13297470+tscr00@users.noreply.github.com>
2018-08-26 18:57:15 +0900

    Add basic drawing methods to FlatCanvas
    Added methods for resetting values of the FlatCanvas and
    changing color of a single pixel.

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,16 @@ pub struct Color {
 }
 
 trait Canvas {
+    fn reset(&mut self);
     fn draw_point(&mut self, point: Point2d, color: Color);
+    fn draw_line(&mut self, from: Point2d, to: Point2d, color: Color);
 }
 
+/// Represents a two-dimensional canvas as flat array of 4 8-bit unsigned integers (RGBA values).
 #[wasm_bindgen]
 pub struct FlatCanvas {
-    width: usize,
-    height: usize,
+    width: i32,
+    height: i32,
     pixels: Vec<Color>,
 }
 
@@ -40,12 +43,41 @@ impl Color {
 #[wasm_bindgen]
 impl FlatCanvas {
 
-    pub fn new(width: usize, height: usize) -> FlatCanvas {
-        let pixels = vec![Color::white(); width*height];
+    pub fn new(width: i32, height: i32) -> FlatCanvas {
+        assert!(width > 0 && height > 0);
+
+        let pixels = vec![Color::white(); (width*height) as usize];
         FlatCanvas { width, height, pixels }
     }
 
     pub fn canvas(&self) -> *const Color {
         self.pixels.as_ptr()
+    }
+
+    fn contains_point(&self, point: &Point2d) -> bool {
+        point.x >= 0 && point.x < self.width && point.y >= 0 && point.y < self.height
+    }
+}
+
+impl Canvas for FlatCanvas {
+
+    fn reset(&mut self) {
+        for pixel in &mut self.pixels {
+            *pixel = Color::white();
+        }
+    }
+
+    fn draw_point(&mut self, point: Point2d, color: Color) {
+        if !self.contains_point(&point) {
+            return
+        }
+
+        let position = point.y * self.width + point.x;
+
+        self.pixels[position as usize] = color;
+    }
+
+    fn draw_line(&mut self, from: Point2d, to: Point2d, color: Color) {
+        unimplemented!();
     }
 }
